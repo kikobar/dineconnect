31ad3d782e075e7472250744832bdf6254de1d6e
Enrique Barcelli <kiko@barcelli.net>
2025-03-07 21:47:22 +0800

    Add config-sample.py

---
+++ config-sample.py
@@ -0,0 +1,14 @@
+# MongoDB Database parameters
+
+CONNECTION_STRING = "mongodb://user:pass@mongoserver:27017/database"
+DATABASE = "outlet"
+COLLECTION = "ticket"
+
+# DineConnect API parameters
+
+api_tenancy_name = "YOUR TENANCY NAME"
+api_username = "YOUR API USERNAME"
+api_password = "YOUR API PASSWORD"
+api_location_id = "YOUR API LOCATION ID"
+base_url = "YOUR API URL"
+
